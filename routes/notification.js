fac1118e0bc1a73861561d19b40e293d5aef569f
Sebastiaan Deckers <seb@ninja.sg>
2015-10-19 21:05:21 +0800

    Hacks!

--- routes/notification.js
+++ routes/notification.js
@@ -4,9 +4,9 @@ var router = express.Router();
 var Pusher = require('pusher');
 var escapeHTML = require('escape-html');
 
-var app_id = process.env.PUSHER_APP_ID;
-var app_key = process.env.PUSHER_APP_KEY;
-var app_secret = process.env.PUSHER_APP_SECRET;
+var app_id = process.env.PUSHER_APP_ID || '148801';
+var app_key = process.env.PUSHER_APP_KEY || '674579b28886235b9637';
+var app_secret = process.env.PUSHER_APP_SECRET || '34df96e2f99abecfe054';
 
 var pusher = new Pusher({
 	appId: app_id,
